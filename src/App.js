233d0a232746e2831dc6764f10d84076515890d7
Wisesa <anvaqta.wisesa@tokopedia.com>
2022-04-20 13:11:28 +0700

    feat: test react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <h1>/ Root</h1>
+          </Route>
+          <Route path="/test" exact>
+            <h1>/test</h1>
+          </Route>
+          <Route path="*">
+            <h1>404 not found</h1>
+          </Route>
+        </Switch>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
